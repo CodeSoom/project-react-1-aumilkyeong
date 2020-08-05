ca7f81aa3774835ff7026cbe379600909a78b783
aumilkyeong <aumilkyeong@gmail.com>
2020-08-05 16:16:36 +0900

    added e2e test for header

--- test/_test.js
+++ test/_test.js
@@ -4,3 +4,11 @@ Scenario('제목을 볼 수 있다', (I) => {
   I.amOnPage('/');
   I.see('Sanchack: Random Street');
 });
+
+Scenario('메뉴를 볼 수 있다', (I) => {
+  I.amOnPage('/');
+  I.see('Home');
+  I.see('History');
+  I.see('Gallery');
+  I.see('FAQ');
+});
