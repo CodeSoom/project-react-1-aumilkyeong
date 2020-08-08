dee6cbb5aba86cad7c8748467f8d3a465615b616
aumilkyeong <aumilkyeong@gmail.com>
2020-08-08 22:50:00 +0900

    can display random google street view panorama

--- src/Components/App.test.jsx
+++ src/Components/App.test.jsx
@@ -2,9 +2,24 @@ import React from 'react';
 
 import { render } from '@testing-library/react';
 
+import { useDispatch, useSelector } from 'react-redux';
+
 import App from './App';
 
+jest.mock('react-redux');
+
 describe('App', () => {
+  const dispatch = jest.fn();
+
+  useDispatch.mockImplementation(() => dispatch);
+
+  useSelector.mockImplementation((selector) => selector({
+    randomSpot: {
+      latitude: 0,
+      longitude: 0,
+    },
+  }));
+
   it('renders well', () => {
     render((
       <App />
