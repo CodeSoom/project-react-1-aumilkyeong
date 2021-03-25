76faa45d4f295afb54c91267dd49759af73c5d1c
aumilkyeong <aumilkyeong@gmail.com>
2021-03-25 11:50:42 +0900

    Build simplest runnable react app

---
+++ src/index.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+function App() {
+    return <p>Hello, React</p>
+}
+
+ReactDOM.render(<App></App>, document.getElementById('app'));
