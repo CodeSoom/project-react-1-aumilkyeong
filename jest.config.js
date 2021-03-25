9454353d69e925d8e9ca06b685a0c8b676986e14
aumilkyeong <aumilkyeong@gmail.com>
2021-03-25 12:15:25 +0900

    Set up unit test
    Chose jest for the test runner with testing libraries.

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+  ],
+  setupFilesAfterEnv: [
+    '<rootDir>/jest.setup.js',
+  ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
+  },
+};
