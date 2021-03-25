d96661f2dec12125473ac0e7880bb1a92633fbd5
aumilkyeong <aumilkyeong@gmail.com>
2021-03-25 12:40:46 +0900

    Set coverage threshold to 100

--- jest.config.js
+++ jest.config.js
@@ -9,4 +9,12 @@ module.exports = {
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': '<rootDir>/node_modules/babel-jest',
   },
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100,
+    },
+  },
 };
