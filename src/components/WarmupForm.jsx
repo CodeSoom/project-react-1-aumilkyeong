46ed37934be09a5f741aa2536c83683fc7d4233f
aumilkyeong <aumilkyeong@gmail.com>
2021-03-30 20:34:06 +0900

    Fix duplicated input id
    Arch Hang can be selected both warmup and strength work. Same ID
    makes strength work's arch hang event not working.

--- src/components/WarmupForm.jsx
+++ src/components/WarmupForm.jsx
@@ -16,21 +16,41 @@ export default function WarmupForm({ config, onChange }) {
     <section id="setting-warmup">
       <h2>준비운동</h2>
       <form data-testid="form-warmup">
-        {Object.entries(config).map(([exercise, isChecked]) => (
-          <label
-            key={exercise}
-            htmlFor={exercise}
-          >
-            <input
-              type="checkbox"
-              id={exercise}
-              name={exercise}
-              checked={isChecked}
-              onChange={onChange}
-            />
-            {labels[exercise]}
-          </label>
-        ))}
+        {Object.entries(config).map(([exercise, isChecked]) => {
+          if (exercise === 'archHang') {
+            const prefixed = `warmup-${exercise}`;
+            return (
+              <label
+                key={exercise}
+                htmlFor={prefixed}
+              >
+                <input
+                  type="checkbox"
+                  id={prefixed}
+                  name={exercise}
+                  checked={isChecked}
+                  onChange={onChange}
+                />
+                {labels[exercise]}
+              </label>
+            );
+          }
+          return (
+            <label
+              key={exercise}
+              htmlFor={exercise}
+            >
+              <input
+                type="checkbox"
+                id={exercise}
+                name={exercise}
+                checked={isChecked}
+                onChange={onChange}
+              />
+              {labels[exercise]}
+            </label>
+          );
+        })}
       </form>
     </section>
   );
