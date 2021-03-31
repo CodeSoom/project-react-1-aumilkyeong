faaa6246bcda0c3960f0cdc25507eced63d243e4
aumilkyeong <aumilkyeong@gmail.com>
2021-03-31 11:11:04 +0900

    Refactor setting page E2E test code
    Removed redundant input selectors. Merge repetitive code using
    functions.

--- cypress/integration/setting-page.spec.js
+++ cypress/integration/setting-page.spec.js
@@ -30,92 +30,48 @@ describe('SettingPage', () => {
     it('has default selection', () => {
       cy.get('[data-testid="form-strengthwork"]')
         .within(() => {
-          cy.get('input[id="pullup"]')
-            .should('be.checked')
-            .get('input[id="squat"]')
-            .should('be.checked')
-            .get('input[id="dip"]')
-            .should('be.checked')
-            .get('input[id="singleLeggedDeadlift"]')
-            .should('be.checked')
-            .get('input[id="horizontalRow"]')
-            .should('be.checked')
-            .get('input[id="pushup"]')
-            .should('be.checked')
-            .get('input[id="plank"]')
-            .should('be.checked')
-            .get('input[id="bandedPallofPress"]')
-            .should('be.checked')
-            .get('input[id="reverseHyperextension"]')
-            .should('be.checked');
+          const defaults = [
+            'pullup',
+            'squat',
+            'dip',
+            'singleLeggedDeadlift',
+            'horizontalRow',
+            'pushup',
+            'plank',
+            'bandedPallofPress',
+            'reverseHyperextension',
+          ];
+
+          const checkDefault = (exercise) => {
+            cy.get(`[id=${exercise}]`)
+              .should('be.checked');
+          };
+
+          defaults.map((exercise) => checkDefault(exercise));
         });
     });
 
     it('can change the selected exercise by category', () => {
-      cy.get('input[id="scapularPull"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="weightedPullup"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="pullup"]:checked')
-        .should('have.length', 1); // 풀업 계열 체크
+      const categories = [
+        'pullup',
+        'squat',
+        'dip',
+        'hinge',
+        'row',
+        'pushup',
+        'antiExtension',
+        'antiRotation',
+        'extension',
+      ];
 
-      cy.get('input[id="assistedSquat"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="shrimpSquat"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="squat"]:checked')
-        .should('have.length', 1); // 스쿼트 계열 체크
+      const checkRadioSelection = (category) => {
+        cy.get(`[name=${category}]`)
+          .click({ multiple: true })
+          .get(`[name=${category}]:checked`)
+          .should('have.length', 1);
+      };
 
-      cy.get('input[id="negativeDip"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="weightedDip"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="dip"]:checked')
-        .should('have.length', 1); // 딥 계열 체크
-
-      cy.get('input[id="romanianDeadlift"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="nordicCurl"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="hinge"]:checked')
-        .should('have.length', 1); // 힌지 계열 체크
-
-      cy.get('input[id="verticalRow"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="weightedInvertedRow"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="row"]:checked')
-        .should('have.length', 1); // 로우 계열 체크
-
-      cy.get('input[id="verticalPushup"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="pseudoPlanchePushup"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="pushup"]:checked')
-        .should('have.length', 1); // 로우 계열 체크
-
-      cy.get('input[id="ringAbRollout"]')
-        .click()
-        .should('be.checked')
-        .get('input[id="plank"]')
-        .click()
-        .should('be.checked')
-        .get('input[name="row"]:checked')
-        .should('have.length', 1); // anti-extension 계열 체크
-
-      // anti-rotation, extension 계열은 운동이 하나라서 생략
+      categories.map((category) => checkRadioSelection(category));
     });
   });
 });
