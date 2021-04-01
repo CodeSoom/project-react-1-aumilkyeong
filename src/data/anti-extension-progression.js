1fde5b1a6ac7d440f9ea14162a9cca2bb8197c2e
aumilkyeong <aumilkyeong@gmail.com>
2021-04-01 18:39:10 +0900

    Add range, demos, and tips information to the core workouts

--- src/data/anti-extension-progression.js
+++ src/data/anti-extension-progression.js
@@ -2,12 +2,27 @@ const plank = {
   level: 1,
   name: 'plank',
   label: 'Plank',
+  range: '30s',
+  demos: ['https://www.youtube.com/watch?v=44ScXWFaVBs&t=10s'],
+  tips: [
+    '3세트 모두 30초를 버틸 수 있다면 다음 단계로',
+    '시범 비디오의 여러 동작 중 플랭크만 하면 됨',
+  ],
 };
 
 const ringAbRollout = {
   level: 2,
   name: 'ringAbRollout',
   label: 'Ring Ab Rollout',
+  range: '8-12',
+  demos: ['https://www.youtube.com/watch?v=LBUfnmugKLw'],
+  tips: [
+    '팔꿈치 곧게 유지',
+    '할로우 바디 자세 유지(갈비뼈 내리고 엉덩이 집어넣기)',
+    '가능한 손 간격을 가까이 두기',
+    '링이 높이가 낮을수록 난이도 증가',
+    '발 높이가 높을수록 난이도 증가',
+  ],
 };
 
 const antiExtensionProgression = [
--- src/data/anti-rotation-progression.js
+++ src/data/anti-rotation-progression.js
@@ -2,6 +2,11 @@ const bandedPallofPress = {
   level: 1,
   name: 'bandedPallofPress',
   label: 'Banded Pallof Press',
+  range: '8-12',
+  demos: ['https://www.youtube.com/watch?v=AH_QZLm_0-s'],
+  tips: [
+    '자세를 제어하면서 팔을 완전히 편 상태에서 잠시 멈춤',
+  ],
 };
 
 const antiRotationProgression = [
