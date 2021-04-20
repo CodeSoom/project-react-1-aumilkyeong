5c7c665c3807e847c6dbc202b26ae5d4824a1cfc
aumilkyeong <aumilkyeong@gmail.com>
2021-04-20 11:32:06 +0900

    Add navigation to home page and workout page

--- src/pages/Setting.jsx
+++ src/pages/Setting.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
 import SettingContainer from '../components/SettingContainer';
 
 export default function SettingPage() {
@@ -7,6 +9,8 @@ export default function SettingPage() {
     <main>
       <h1>Setting</h1>
       <SettingContainer />
+      <Link to="/">To the Introduction</Link>
+      <Link to="/workout">To the Workout of the day</Link>
     </main>
   );
 }
